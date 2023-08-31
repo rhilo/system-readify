a82e7d7058145a50339e10ab39c02a51a48bb1c9
Christer Karlsen <admin@easymail.no>
2023-08-31 05:26:57 +0200

    did stuff to prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -3,23 +3,19 @@
 # Run apt update && apt upgrade in the background and suppress output
 apt update > /dev/null 2>&1 && apt upgrade -y > /dev/null 2>&1 &
 
-# Prepare Docker Compose directories
-mkdir -p compose/{postgresql,mysql,mongodb,apache,nginx,redis}
+# Prepare Docker Compose directories one by one
+echo "Creating compose directory..."
+mkdir -p compose
 
-# Create docker-compose.yml files in each directory
-touch compose/postgresql/docker-compose.yml
-touch compose/mysql/docker-compose.yml
-touch compose/mongodb/docker-compose.yml
-touch compose/apache/docker-compose.yml
-touch compose/nginx/docker-compose.yml
-touch compose/redis/docker-compose.yml
+echo "Creating service directories and files..."
+for service in postgresql mysql mongodb apache nginx redis
+do
+    mkdir -p "compose/$service"
+    touch "compose/$service/docker-compose.yml"
+done
 
 # Wait for the background job to finish (the apt update && apt upgrade)
 wait
 
 # Print success message
 echo "OK, INITIAL PREPARATION DONE"
-
-# Invoke Python script for further installation
-echo "Running Python installation script..."
-python3 setup.py
